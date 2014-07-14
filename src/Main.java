549d317003c45300d12b938d05f7b3ada13095c4
amelia-ylh <amelia_ylh@hotmail.com>
2014-07-14 10:45:36 -0700

    idk what i did...

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		String textFile = readFile("src/alice30.txt");
+		System.out.println(textFile);
 		buildCharacterToFrequencyMap(textFile);
 
 	}
